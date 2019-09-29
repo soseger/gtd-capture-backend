b33373ca1d51fef84364541152004afff2b14ae4
soseger <katjasofiaseger@gmail.com>
2019-09-29 15:52:51 +0300

    Impemented email sending

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-var express = require("express");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helper = require('sendgrid').mail;
+const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
-var app = express();
+const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get("/test", (req, res) => {
-  res.send("test");
+app.post('/', (req, res) => {
+  const from_email = new helper.Email('noreply@to.do');
+  const to_email = new helper.Email(req.body.email);
+  const subject = `TODO: ${req.body.message.substring(0, 50)}...`;
+  const content = new helper.Content('text/plain', req.body.message);
+  const mail = new helper.Mail(from_email, subject, to_email, content);
+
+  const request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+
+  sg.API(request, function(error, response) {
+    if (error) {
+      console.error(error);
+      return res.status(error.response.statusCode).send(error.message);
+    }
+    return res.status(response.statusCode).end();
+  });
 });
 
 module.exports = app;
